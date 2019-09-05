f6c86d7cc9318e55365d707190bb56b5967dcdad
Johnson Ojo <johnsonojo89@gmail.com>
2019-09-05 20:16:16 +0100

    chore(setup): aslant setup
    add eslint setup to lint the files
    
    [Finishes #168287915]

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) =>
-  res.status(200).json({
-    status: "success",
-    message: "welcome to fast food fast"
-  })
-);
+app.get('/', (req, res) => res.status(200).json({
+  status: 'success',
+  message: 'welcome to fast food fast'
+}));
 
 app.listen(PORT, () => console.log(`fast food fast is listening on ${PORT}`));
 
