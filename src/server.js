56b70c967251c397d83934b3842c3c0cdd0e96ec
Johnson Ojo <johnsonojo89@gmail.com>
2019-09-05 19:52:39 +0100

    chore(setup): babel server
    add babel setup to transpire ES6 to ES5
    
    [Finishes #168287923]

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
-// import express from "express";
-const express = require("express");
+import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,5 +12,4 @@ app.get("/", (req, res) =>
 
 app.listen(PORT, () => console.log(`fast food fast is listening on ${PORT}`));
 
-// export default app;
-module.exports = app;
+export default app;
