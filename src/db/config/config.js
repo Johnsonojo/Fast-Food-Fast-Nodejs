7100adbe51931eb804908c1536100fc287d2b4ac
Johnson Ojo <johnsonojo89@gmail.com>
2019-09-07 07:45:30 +0100

    chore(setup): postgres setup
    add setup for Postgres and sequelize ORM
    
    [Finishes #168287896]

---
+++ src/db/config/config.js
@@ -0,0 +1,21 @@
+require('babel-register');
+const dotenv = require('dotenv');
+// import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dialect = 'postgres';
+
+module.exports = {
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL_TEST',
+    dialect
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL'
+  }
+};
