f61fdc9f168649f65fd93a2d389dfa1ff4220d7c
Johnson Ojo <32887459+Johnsonojo@users.noreply.github.com>
2019-10-08 14:14:41 +0100

    feature(user): update user profile functionality (#16)
    - add function to update user profile
    - add route to update user profile
    
    [Delivers #168556235]

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import getCurrentUser from '../controllers/ProfileController';
+import { getCurrentUser, editUserProfile } from '../controllers/ProfileController';
 import { verifyToken } from '../middlewares/authenticator';
+import {
+  checkImageUrl, checkUndefined, checkEmpty, checkLength
+} from '../middlewares/profileInputValidator';
 
 const profileRoute = express.Router();
 
 profileRoute.get('/user', verifyToken, getCurrentUser);
+profileRoute.put('/user',
+  verifyToken, checkUndefined,
+  checkEmpty, checkLength,
+  checkImageUrl, editUserProfile);
 
 export default profileRoute;
